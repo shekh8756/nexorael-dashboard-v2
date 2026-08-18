02e7e3c4ede092598026f313325d115d6b3c16a3
Shekh Shahid <email212shahid@gmail.com>
2026-08-18 13:54:39 +0530

    Fix Vercel FFmpeg binary deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  outputFileTracingIncludes: {
+    "/*": [
+      "./node_modules/ffmpeg-static/ffmpeg",
+    ],
+  },
+
+  serverExternalPackages: [
+    "ffmpeg-static",
+  ],
 };
 
 export default nextConfig;
